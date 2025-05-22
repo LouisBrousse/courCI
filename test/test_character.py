a0d5e54b24605ef3438892ec7e0a83bb48543f7e
Ornella <ornella@Ornellas-MacBook-Pro.local>
2025-05-22 10:47:33 +0200

    test success ci

--- test/test_character.py
+++
@@ -1,6 +0,0 @@
-from rpg.character import Character
-
-
-def test_character_starts_with_10_hp():
-    hero = Character("Hero")
-    assert hero.hp == 10
--- test/test_env_failure.py
+++ test/test_env_failure.py
@@ -1,3 +1,3 @@
 # test failure in ci
 def test_true_is_true():
-    assert True is False
+    assert True is True
