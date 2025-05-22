8cc912ebc4522f5585cdd3f97e88b72bf2b126ed
m-ornella <135030958+m-ornella@users.noreply.github.com>
2025-05-22 13:30:53 +0200

    Update test_character.py

--- test/test_character.py
+++ test/test_character.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from src.character import Character
 
 def test_character_starts_with_10_hp():
